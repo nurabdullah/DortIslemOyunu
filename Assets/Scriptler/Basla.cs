5c77ccfa3433ccc78dcfe88f45c701c9d9dcd92e
nurabdullah <abdullahnur.ybs@gmail.com>
2020-10-20 16:37:24 +0300

    oyun tamamlandı

---
+++ Assets/Scriptler/Basla.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+   public class Basla : MonoBehaviour
+{
+
+    public void digersahne()
+    {
+        SceneManager.LoadScene("oyun");
+
+    }
+   
+    public void cıkıs()
+    {
+        Application.Quit();
+
+    }
+}
---
+++ Assets/Scriptler/dortııslem.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class dortııslem : MonoBehaviour
+{
+    public UnityEngine.UI.InputField sonucInput;
+    public Text ilksayi,islem,ikincisayi, esittir,cevap, sonuc;
+    int sayi1,sayi2,ıslemısareti;
+    int islemsonucu;
+    int maxsayi = 10;
+
+    void Start()
+    {
+        YeniSoru();
+    }
+
+
+     public void cevapKontrol()
+    {
+        if (int.Parse(cevap.text) == islemsonucu)
+        {
+            sonuc.text = "DOGRU";
+
+
+
+        }
+        else { 
+            sonuc.text = "YANLIS";
+        }
+    }
+
+    public void YeniSoru()
+    {
+        sonuc.text = "";
+        sonucInput.text = "";
+        sayi1 = Random.Range(1, 20);
+        sayi2 = Random.Range(1, 10);
+        ıslemısareti = Random.Range(1, 5);
+
+        switch (ıslemısareti)
+        {
+            case 1:
+                islem.text = "+";
+                islemsonucu = sayi1 + sayi2;
+                break;
+            case 2:
+                islem.text = "-";
+                islemsonucu = sayi1 - sayi2;
+                break;
+            case 3:
+                islem.text = "/";
+                islemsonucu = sayi1 / sayi2;
+                break;
+            case 4:
+                islem.text = "*";
+                islemsonucu = sayi1 * sayi2;
+                break;
+        }
+        ilksayi.text = sayi1 + "";
+        ikincisayi.text = sayi2 + "";
+
+
+
+
+    }
+}
